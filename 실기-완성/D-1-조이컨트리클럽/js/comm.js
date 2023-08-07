01eabc24093063b9113e740bb08cdcd0481c1e96
jiseonn97 <jiseonn97@naver.com>
2023-08-07 15:13:34 +0900

    slide, quickmenu

--- 실기-완성/D-1-조이컨트리클럽/js/comm.js
+++ 실기-완성/D-1-조이컨트리클럽/js/comm.js
@@ -1,3 +1,19 @@
 $(function () {
-  // tabs
+    // $("#gnb .link-depth1").on("click", function () {
+    //     $(".depth1 > li").removeClass("active");
+    //     $(this).parent().addClass("active");
+    // });
+
+    // // //조건문
+
+    $("#gnb .link-depth1").on("click", function () {
+        if ($(this).parent().hasClass("active")) {
+            // 만약 active 가지고 있다면
+            $(this).parent().removeClass("active");
+        } else {
+            // 가지고있지않다면
+            $(".depth1 > li").removeClass("active");
+            $(this).parent().addClass("active");
+        }
+    });
 });
